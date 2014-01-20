313fd849fc1335550403446df0d28d0d6d7272a9
hanse <thehanse@gmail.com>
2014-01-20 13:51:13 +0100

    typo

--- exercise3/src/MainPanel.java
+++ exercise3/src/MainPanel.java
@@ -1,10 +1,11 @@
 
 import java.awt.Component;
+import java.awt.Container;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import weka.gui.visualize.Plot2d;
+import weka.gui.visualize.Plot2D;
 
 public class MainPanel extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -17,12 +18,21 @@ public class MainPanel extends JFrame {
        
        Container pane = this.getContentPane();
        
+       createPlot(pane);
        JButton button = new JButton("test");
        button.setAlignmentX(Component.CENTER_ALIGNMENT);
        pane.add(button);
        
        setDefaultCloseOperation(EXIT_ON_CLOSE);        
     }
+	
+	private void createPlot(Container container)
+	{
+		Plot2D plot = new Plot2D();
+		
+		
+		container.add(plot);
+	}
 
     public static void main(String[] args) {
         
