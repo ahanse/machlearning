02edfeee240d9380982f7b324db8abd1536c34b6
hanse <thehanse@gmail.com>
2014-01-20 13:40:14 +0100

    button

--- exercise3/src/MainPanel.java
+++ exercise3/src/MainPanel.java
@@ -1,23 +1,28 @@
 
+import java.awt.Component;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import weka.gui.visualize.Plot2d;
 
 public class MainPanel extends JFrame {
-
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
 
 	public MainPanel() {
         
        setTitle("AdaBoost example");
        setSize(600, 400);
        setLocationRelativeTo(null);
+       
+       Container pane = this.getContentPane();
+       
+       JButton button = new JButton("test");
+       button.setAlignmentX(Component.CENTER_ALIGNMENT);
+       pane.add(button);
+       
        setDefaultCloseOperation(EXIT_ON_CLOSE);        
     }
-    
 
     public static void main(String[] args) {
         
