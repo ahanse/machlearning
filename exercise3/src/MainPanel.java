1166eb21fd2be51f58eae2fed99f75a79d8bd3b3
hanse <thehanse@gmail.com>
2014-01-20 14:12:15 +0100

    open file

--- exercise3/src/MainPanel.java
+++ exercise3/src/MainPanel.java
@@ -1,37 +1,53 @@
 
-import java.awt.Component;
-import java.awt.Container;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 import weka.gui.visualize.Plot2D;
+import weka.gui.visualize.PlotData2D;
+import weka.core.Instances;
 
-public class MainPanel extends JFrame {
+public class MainPanel extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
-	public MainPanel() {
-        
+	private JButton openButton;
+    private JFileChooser fc;
+    private Instances instances;
+    private Plot2D plot;
+	
+	public MainPanel() {     
        setTitle("AdaBoost example");
        setSize(600, 400);
        setLocationRelativeTo(null);
+       fc = new JFileChooser();
        
        Container pane = this.getContentPane();
+       pane.setLayout(new BoxLayout(pane, BoxLayout.LINE_AXIS));
        
        createPlot(pane);
-       JButton button = new JButton("test");
-       button.setAlignmentX(Component.CENTER_ALIGNMENT);
-       pane.add(button);
+       pane.add(Box.createRigidArea(new Dimension(0,5)));  
+       createOptionPane(pane);
        
        setDefaultCloseOperation(EXIT_ON_CLOSE);        
     }
 	
 	private void createPlot(Container container)
 	{
-		Plot2D plot = new Plot2D();
-		
+		plot = new Plot2D();
 		
 		container.add(plot);
+	}
+	
+	private void createOptionPane(Container container)
+	{
+	       openButton = new JButton("Open Dataset");
+	       openButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+	       openButton.addActionListener(this);
+	       container.add(openButton);
 	}
 
     public static void main(String[] args) {
@@ -44,4 +60,25 @@ public class MainPanel extends JFrame {
             }
         });
     }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == openButton) {
+            int returnVal = fc.showOpenDialog(MainPanel.this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+				try {
+					BufferedReader reader = new BufferedReader(new FileReader(file));
+					instances = new Instances(reader);
+					plot.addPlot(new PlotData2D(instances));
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+                //This is where a real application would open the file.
+            } else {
+            }
+        }
+	}
 }
